fcc7297942db94757ec575015eceed73e5e01269
jindacz <jindaz.work@outlook.com>
2023-02-22 01:09:27 -0800

    main page render lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Post from './components/Post'
 
-function App() {
-  const [count, setCount] = useState(0)
+// prop = {name: value, comment: value, numLikes: value}
 
+const App = () => {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+           <h1>Food track favoruites</h1>
+      {/* <h3>Write a post about what you are excited to learn about!</h3> */}
+      <Post name="Marques Brownlee" comment="I LOVE CS" numLikes="100"/>
+      <Post name="William McPhail" comment=":crickets:" numLikes="100000"/>
     </div>
   )
 }
-
-export default App
+export default App;
