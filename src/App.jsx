67bd41926e0289a691eaeda8c2d15611842fb01d
jindacz <jindaz.work@outlook.com>
2023-02-26 23:36:58 -0800

    make card grid

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,21 @@ import Post from './components/Post'
 const App = () => {
   return (
     <div className="App">
-           <h1>Food track favoruites</h1>
+      <h1>Food track favoruites</h1>
       {/* <h3>Write a post about what you are excited to learn about!</h3> */}
-      <Post name="Birria-Landia" location="Mexican" img={'./images/img01.jpg'}/>
-      <Post name="Mysttik Masala" location="Indian"/>
-      <Post name="The Halal Guys" location="Middle Eastern"/>
-      <Post name="The Halal Guys" location="Middle Eastern"/>
-      <Post name="The Halal Guys" location="Middle Eastern"/>
-      <Post name="The Halal Guys" location="Middle Eastern"/>
-      <Post name="The Halal Guys" location="Middle Eastern"/>
-      <Post name="The Halal Guys" location="Middle Eastern"/>
-      <Post name="The Halal Guys" location="Middle Eastern"/>
-      <Post name="The Halal Guys" location="Middle Eastern"/>
-      <Post name="The Halal Guys" location="Middle Eastern"/>
-      
+      <div className="PostList">
+        <Post name="Birria-Landia" location="Mexican" img={'./images/img01.jpg'}/>
+        <Post name="Mysttik Masala" location="Indian"/>
+        <Post name="The Halal Guys" location="Middle Eastern"/>
+        <Post name="The Halal Guys" location="Middle Eastern"/>
+        <Post name="The Halal Guys" location="Middle Eastern"/>
+        <Post name="The Halal Guys" location="Middle Eastern"/>
+        <Post name="The Halal Guys" location="Middle Eastern"/>
+        <Post name="The Halal Guys" location="Middle Eastern"/>
+        <Post name="The Halal Guys" location="Middle Eastern"/>
+        <Post name="The Halal Guys" location="Middle Eastern"/>
+        <Post name="The Halal Guys" location="Middle Eastern"/>
+      </div>
 
     </div>
   )
